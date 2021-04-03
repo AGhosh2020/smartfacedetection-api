cba5d902153287a1b4fe993135d3ce2f42354e0d
AGhosh2020 <66912187+AGhosh2020@users.noreply.github.com>
2021-04-03 20:36:31 +0530

    controllers

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+const  Clarifai = require('clarifai');
+const app = new Clarifai.App({
+    apiKey: 'da823175b0fa4005b5f6a0cec2aa8890'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict("d02b4508df58432fbb84e800597b8959",req.body.input)   
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with clarifai"));
+}
+
+
+const handleImage = (req, res, db) =>{
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1).returning('entries').then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"))
+}
+module.exports = {
+    handleImage,
+    handleApiCall
+}
